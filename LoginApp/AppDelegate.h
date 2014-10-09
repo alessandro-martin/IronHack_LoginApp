a0725d5d1b71bf54f07938306be33c66facac3de
alessandro-martin <martin.acg@gmail.com>
2014-10-09 11:49:52 +0200

    Initial Commit

---
+++ LoginApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginApp
+//
+//  Created by Alessandro on 09/10/14.
+//  Copyright (c) 2014 Alessandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginApp
+//
+//  Created by Alessandro on 09/10/14.
+//  Copyright (c) 2014 Alessandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
